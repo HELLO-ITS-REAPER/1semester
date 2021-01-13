127db8ee28e00f6c8b5414798d72ac884169d380
Martin Kjeldsen <martindkjeldsen@gmail.com>
2021-01-13 10:06:22 +0100

    OldDataRepository & Repository cs files created.

---
+++ CoronaApp/OldDataRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace CoronaApp
+{
+    class OldDataRepository : Repository
+    {
+        protected SqlConnection connection = null;
+        public void Search()
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM dbo.Cumulative_incidents", connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in OldDataRepository " + ex.Message);
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open) connection.Close();
+            }
+        }
+    }
+}
